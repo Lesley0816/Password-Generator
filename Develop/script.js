8ecf4da0064b79d61fcc7b8a2991b3868801f12d
Lesley McGregor <mcgregorlesley@gmail.com>
2024-02-12 22:17:03 -0600

    added first window prompt

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,28 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+const button = document.querySelector('button');
+
+
+button.addEventListener('click', function() {
+
+  const userInput = prompt("How many characters?")  
+  
+  console.log(userInput);
+  if (userInput === ""){
+    return;
+  }
+  else if (parseInt(userInput, 10) < 8) {
+    alert("Number is too low")
+    return;
+  }
+  else if (parseInt(userInput, 10) > 128) {
+    alert("Number is too high")
+    return;
+  }
+  const userChoice1 = confirm("Do you want Uppercase Characters")
+  if (userChoice1 == true) {
+    
+  }
+});
